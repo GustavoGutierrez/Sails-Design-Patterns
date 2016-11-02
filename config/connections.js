f942e0dc254d30589c1856e18e4abcacbbcd3359
Gustavo Gutierrez <gustavogutierrezmercado@gmal.com>
2016-11-01 23:44:33 -0500

    Inicio de proyecto

--- config/connections.js
+++ config/connections.js
@@ -1,10 +1,10 @@
 module.exports.connections = {
  mongoDb: {
     adapter: 'sails-mongo',
-    host: 'r2.ecs.network', // defaults to `localhost` if omitted
+    host: 'yourhost', // defaults to `localhost` if omitted
     port: 27017, // defaults to 27017 if omitted
-    user: 'NodrizaDev', // or omit if not relevant
-    password: 'shox009009', // or omit if not relevant
-    database: 'NodrizaDev' // or omit if not relevant
+    user: 'yourusername', // or omit if not relevant
+    password: 'yourpassword', // or omit if not relevant
+    database: 'yourdatabase' // or omit if not relevant
   }
 };
