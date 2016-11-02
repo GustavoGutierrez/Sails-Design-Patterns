0f5ce7eba78b586c6c2ab932db66f561bac3ac98
Gustavo Gutierrez <gustavogutierrezmercado@gmal.com>
2016-11-01 23:59:31 -0500

    env

--- config/connections.js
+++ config/connections.js
@@ -1,10 +1,10 @@
 module.exports.connections = {
  mongoDb: {
     adapter: 'sails-mongo',
-    host: 'yourhost', // defaults to `localhost` if omitted
-    port: 27017, // defaults to 27017 if omitted
-    user: 'yourusername', // or omit if not relevant
-    password: 'yourpassword', // or omit if not relevant
-    database: 'yourdatabase' // or omit if not relevant
+    host: process.env.DB_HOST, // defaults to `localhost` if omitted
+    port: process.env.DB_PORT, // defaults to 27017 if omitted
+    user: process.env.DB_USERNAME, // or omit if not relevant
+    password: process.env.DB_PASSWORD, // or omit if not relevant
+    database: process.env.DB_DATABASE // or omit if not relevant
   }
 };
